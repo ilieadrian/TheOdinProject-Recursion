8f3ac55e1f580121ae220c06bde615ce43da516e
I A <ilie.adrian1986@gmail.com>
2024-11-22 14:22:49 +0200

    fibonacci iterative version completed

--- rec.js
+++ rec.js
@@ -1,6 +1,22 @@
 //1.Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the Fibonacci sequence. 
 //Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+function fibs(num) {
+    let fib = [0, 1]
 
-function fibs(arr) {
-    
-}
+    if (num <= 1){
+        fib = num;
+    }
+  
+    for(let i = 2; i < num; i++){
+        const a = fib[i - 1];
+        const b = fib[i - 2];
+        
+        fib.push(a + b)
+    }
+    console.log(fib);
+    }
+
+fibs(8)
+
+
+
